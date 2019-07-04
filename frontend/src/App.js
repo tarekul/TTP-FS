305c8b319459d48c1f9b53813cc776e8300367c2
Tarek <bayridge4th@gmail.com>
2019-07-04 17:45:37 -0400

    setup register page to portfolio if user registered

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import {HashRouter, Route} from 'react-router-dom'
-
+import firebase from './firebase'
 import Register from './containers/register'
 import Nav from './containers/nav'
 import Portfolio from './containers/portfolio'
+import AuthContext from './contexts/authContext'
 
-function App() {
-  return (
-    <HashRouter>
-      <Route path='/' component={Nav}/>
-      <Route path='/register' exact component={Register}/>
-      <Route path='/portfolio' exact component={Portfolio}/>
-    </HashRouter>
-  );
+class App extends React.Component{
+  state = {
+    user:null
+  }
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user=>this.setState({user}))
+  }
+  render(){
+    const {user} = this.state
+    console.log(user)
+    return (
+      <HashRouter>
+        <AuthContext.Provider value={user}>
+          <Route path='/' component={Nav}/>
+          <Route path='/register' exact component={Register}/>
+          <Route path='/portfolio' exact component={Portfolio}/>
+        </AuthContext.Provider>
+      </HashRouter>
+    );
+  }
+  
 }
 
 export default App;
--- frontend/src/containers/register.js
+++ frontend/src/containers/register.js
@@ -4,8 +4,11 @@ import RegForm from '../components/regform'
 import Validateform from '../components/validateform'
 import RegisterError from '../components/registererror'
 import firebase from '../firebase'
+import AuthContext from '../contexts/authContext'
+
 
 class Register extends React.Component{
+    static contextType = AuthContext
     state = {
         firstname:'',
         lastname:'',
@@ -15,6 +18,10 @@ class Register extends React.Component{
         error:null
     }
 
+    componentDidMount(){
+        //console.log(this.context)
+        if(this.context) this.props.history.push('/portfolio')
+    }
     onChange = (e)=>{
         this.setState({[e.target.id]:e.target.value})
         
@@ -25,8 +32,8 @@ class Register extends React.Component{
         const {firstname,lastname,email,password} = this.state
         if(firstname && lastname && email && password) {
             firebase.auth().createUserWithEmailAndPassword(email,password)
-            .then(()=>this.setState({validated:true,error:null}))
-            .catch(err => this.setState({error:err.message}))
+            .then(()=>this.props.history.push('/portfolio'))
+            .catch(err => this.setState({validated:true,error:err.message}))
         }
         else this.setState({validated:false,error:null})
     }
@@ -38,14 +45,14 @@ class Register extends React.Component{
         const alert2 = error ? <RegisterError error={error} /> : ''
         
         return <>
-        <div class='container mt-5 jumbotron'>
-        <div style={{minHeight:'10vh'}}>
-            {alert}
-            {alert2}
-        </div>
-        <RegForm onChange={this.onChange} onSubmit={this.onSubmit}/>
-        </div>
-    </>
+            <div class='container mt-5 jumbotron'>
+            <div style={{minHeight:'10vh'}}>
+                {alert}
+                {alert2}
+            </div>
+            <RegForm onChange={this.onChange} onSubmit={this.onSubmit}/>
+            </div>
+        </>
     }
     
 }
