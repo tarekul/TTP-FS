163a0b016586ebadf5f1450b1b9624acaae443ad
Tarek <bayridge4th@gmail.com>
2019-07-08 15:00:45 -0400

    minor edit to regform

--- frontend/src/components/regform.js
+++ frontend/src/components/regform.js
@@ -5,20 +5,20 @@ const RegForm = (props) =>{
     <div className="form-row">
         <div className="form-group col-md-6">
             <label for="inputEmail4">First Name</label>
-            <input type="text" className="form-control"  placeholder="firstname" id='firstname'  onChange={e=>{this.props.onChange(e)}} />
+            <input type="text" className="form-control"  placeholder="firstname" id='firstname'  onChange={e=>{props.onChange(e)}} />
         </div>
         <div className="form-group col-md-6">
             <label for="inputEmail4">Last Name</label>
-            <input type="text" className="form-control"  placeholder="lastname" id='lastname'  onChange={e=>{this.props.onChange(e)}} />
+            <input type="text" className="form-control"  placeholder="lastname" id='lastname'  onChange={e=>{props.onChange(e)}} />
         </div>
 
         <div className="form-group col-md-6">
             <label for="inputEmail4">Email</label>
-            <input type="email" className="form-control" id="inputEmail4" placeholder="Email" id='email'  onChange={e=>{this.props.onChange(e)}}/>
+            <input type="email" className="form-control" id="inputEmail4" placeholder="Email" id='email'  onChange={e=>{props.onChange(e)}}/>
         </div>
         <div className="form-group col-md-6">
             <label for="inputPassword4">Password</label>
-            <input type="password" className="form-control" id="inputPassword4" placeholder="Password" id='password'  onChange={e=>{this.props.onChange(e)}}/>
+            <input type="password" className="form-control" id="inputPassword4" placeholder="Password" id='password'  onChange={e=>{props.onChange(e)}}/>
         </div>
     </div>    
     <div className='submit-container'>
